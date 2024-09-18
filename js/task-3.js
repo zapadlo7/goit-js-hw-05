c2d68ed095e2ab41d841c84db4f16b077089e0b7
Yevhen_B <zapadlo222@gmail.com>
2024-09-18 11:36:39 +0300

    fix task3

--- js/task-3.js
+++ js/task-3.js
@@ -1,6 +1,6 @@
 'use strict';
 const sortByDescendingFriendCount = users => {
-  return users.sort((a, b) => b.friends.length - a.friends.length);
+  return users.toSorted((a, b) => b.friends.length - a.friends.length);
 };
 
 console.log(
